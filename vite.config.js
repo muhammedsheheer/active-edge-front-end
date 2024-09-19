013760c2d7dfaa10a2f33245993443b792df6ae0
sheheer <muhammedsheheer99@gmail.com>
2024-09-20 00:17:30 +0530

    second commit vite updated

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,23 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// export default defineConfig({
+// 	plugins: [react()],
+// 	server: {
+// 		port: 3000,
+// 	},
+// });
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
 	server: {
 		port: 3000,
+	},
+	build: {
+		outDir: "dist",
+		chunkSizeWarningLimit: 1000,
 	},
 });
